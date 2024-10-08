c3394e77f41fd3df79182850af285cf7ff2733ba
Yasmin Ferreira <yasmincferreira8@gmail.com>
2024-10-08 00:12:50 -0400

    criando a rota livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1, 
+        titulo: "Hobbit"
+    },
+    {
+        id: 2, 
+        titulo: "Senhor dos Aneis"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de NodeJS, API Rest");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app;
